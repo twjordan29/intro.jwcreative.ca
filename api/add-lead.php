c3cf8cbfec5e348df72d1185d67536992aa00994
MaplrCanada <maplrrcanada@gmail.com>
2025-06-28 16:56:15 -0230

    Update add-lead.php

--- api/add-lead.php
+++ api/add-lead.php
@@ -12,13 +12,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $demo1Title = $_POST['demo1Title'] ?? '';
     $demo2Title = $_POST['demo2Title'] ?? '';
     $demo3Title = $_POST['demo3Title'] ?? '';
-    $demo1Desc = $_POST['demo1Desc'] ?? '';
     $demo1Image = $_POST['demo1Image'] ?? '';
     $demo1Url = $_POST['demo1Url'] ?? '';
-    $demo2Desc = $_POST['demo2Desc'] ?? '';
     $demo2Image = $_POST['demo2Image'] ?? '';
     $demo2Url = $_POST['demo2Url'] ?? '';
-    $demo3Desc = $_POST['demo3Desc'] ?? '';
     $demo3Image = $_POST['demo3Image'] ?? '';
     $demo3Url = $_POST['demo3Url'] ?? '';
     $interested = "Waiting for response";
@@ -30,8 +27,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit;
     }
 
-    $stmt = $conn->prepare("INSERT INTO leads (client_name, slug, original_price, offer_price, demo1_type, demo1_title, demo1_desc, demo1_image, demo1_url, demo2_type, demo2_title, demo2_desc, demo2_image, demo2_url, demo3_type, demo3_title, demo3_desc, demo3_image, demo3_url, interested) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssddssssssssssssssss", $clientName, $slug, $originalPrice, $offerPrice, $demo1Type, $demo1Title, $demo1Desc, $demo1Image, $demo1Url, $demo2Type, $demo2Title, $demo2Desc, $demo2Image, $demo2Url, $demo3Type, $demo3Title, $demo3Desc, $demo3Image, $demo3Url, $interested);
+    $stmt = $conn->prepare("INSERT INTO leads (client_name, slug, original_price, offer_price, demo1_type, demo1_title, demo1_image, demo1_url, demo2_type, demo2_title, demo2_image, demo2_url, demo3_type, demo3_title, demo3_image, demo3_url, interested) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssddsssssssssssss", $clientName, $slug, $originalPrice, $offerPrice, $demo1Type, $demo1Title, $demo1Image, $demo1Url, $demo2Type, $demo2Title, $demo2Image, $demo2Url, $demo3Type, $demo3Title, $demo3Image, $demo3Url, $interested);
 
     if ($stmt->execute()) {
         echo "Lead added successfully.";
